4cddbb35df457e998e08b32da8c55d0aaec8ca7b
zhaojianbo <18257346778@163.com>
2019-09-01 12:41:55 +0800

    yidiandian

--- src/views/register.vue
+++ src/views/register.vue
@@ -4,12 +4,21 @@
 
         <div class="login-form">
             <ul>
-            <li><div class="ipt"><span class="ico"><img src="static/img/d2.png"></span><input type="text" value="" placeholder="请输入手机号"></div></li>
-            <li><div class="ipt"><span class="ico"><img src="static/img/d3.png"></span><input type="text" value="" placeholder="请输入密码"></div></li>
-            <li><div class="ipt"><span class="ico"><img src="static/img/d4.png"></span><input type="text" value="" placeholder="请输入验证码" style="width:120px;"><div class="send-code">发送验证码</div></div></li>
-            <li><div class="ipt"><span class="ico"><img src="static/img/d4.png"></span><input type="text" value="" placeholder="请输入验证码" style="width:120px;"><div class="imgcode"><img src="static/img/cod.jpg"></div></div></li>
+            <li>
+                <div class="ipt"><span class="ico"><img src="static/img/d2.png"></span>
+                <input type="text" value="" placeholder="请输入手机号"></div></li>
+            <li>
+                <div class="ipt"><span class="ico"><img src="static/img/d3.png"></span><input type="text" value="" placeholder="请输入密码">
+                </div></li>
+            <li><div class="ipt"><span class="ico"><img src="static/img/d4.png"></span>
+                <input type="text" value="" placeholder="请输入验证码" style="width:120px;"><div class="send-code" @click="sendAuthCode">发送验证码</div>
+            </div></li>
+            <li><div class="ipt"><span class="ico"><img src="static/img/d4.png"></span>
+                <input type="text" value="" placeholder="请确认密码" style="width:120px;"><div class="imgcode"><img src="static/img/cod.jpg">
+                </div></div></li>
 
-            <li style="position:relative;"><input id="checkbox-blue" type="checkbox" class="checkbox"><label for="checkbox-blue"></label ><a style="padding-left:12px;">同意并愿意遵守星座世界<span>使用条款</span></a></li>
+            <li style="position:relative;">
+                <input id="checkbox-blue" type="checkbox" class="checkbox"><label for="checkbox-blue"></label ><a style="padding-left:12px;">同意并愿意遵守星座世界<span>使用条款</span></a></li>
             <li><div class="submit"><input type="submit" value="注册"></div></li>
             <li><div class="left"><a href="#">已有账号！<span>立即登录</span></a></div>
             </li>
@@ -20,7 +29,12 @@
 
 <script>
   export default {
-    name: "register"
+    name: "register",
+    methods: {
+      sendAuthCode () {
+
+      }
+    }
   }
 </script>
 
